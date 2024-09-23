710735ba8fed6b3962ac08f2537917d44dcd8928
James Albright <G00379271@atu.ie>
2024-09-23 15:47:54 +0100

    Added Post request to create a product and a get request to retrieve the list of products

--- src/main/java/ie/atu/week2_refresher/Week2Refresher.java
+++ src/main/java/ie/atu/week2_refresher/Week2Refresher.java
@@ -2,8 +2,14 @@ package ie.atu.week2_refresher;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class Week2Refresher {
+
+    private List<Product> productList = new ArrayList<>();
+
     @GetMapping("/hello")
     public String message() {
         return "hello";
@@ -19,6 +25,15 @@ public class Week2Refresher {
         return search1 + " " + search2;
     }
 
+    @GetMapping("/products")
+    public List<Product> getProductList() {
+        return productList;
+    }
 
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        productList.add(product);
+        return product;
+    }
 
 }
