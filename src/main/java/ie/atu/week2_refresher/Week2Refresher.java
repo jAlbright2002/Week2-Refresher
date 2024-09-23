5eb43aec90508df68ca35cf434f748a02b8632cf
James Albright <G00379271@atu.ie>
2024-09-23 15:42:21 +0100

    Created new methods and new Product class

--- src/main/java/ie/atu/week2_refresher/Week2Refresher.java
+++ src/main/java/ie/atu/week2_refresher/Week2Refresher.java
@@ -1,4 +1,24 @@
 package ie.atu.week2_refresher;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class Week2Refresher {
+    @GetMapping("/hello")
+    public String message() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String myName(@PathVariable String name) {
+        return "hello, " + name;
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam String search1, @RequestParam String search2) {
+        return search1 + " " + search2;
+    }
+
+
+
 }
