3c887d577b35bf72775e22b0bce33df4a63e56d7
James Albright <G00379271@atu.ie>
2024-09-23 15:28:38 +0100

    New Project

---
+++ src/main/java/ie/atu/week2_refresher/Week2Refresher.java
@@ -0,0 +1,4 @@
+package ie.atu.week2_refresher;
+
+public class Week2Refresher {
+}
---
+++ src/test/java/ie/atu/week2_refresher/Week2RefresherApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2_refresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2RefresherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
